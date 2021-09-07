7c5ce0d60b6ed31b73006e5d99861c523682df66
Andres Duarte <adc100784@gmail.com>
2021-09-07 08:22:32 -0600

    Ejercicios 34 y 35

---
+++ ex34.py
@@ -0,0 +1,7 @@
+animals = ['bear', 'python3.9', 'peacok', 'kangaroo', 'whale', 'platypus']
+
+print(f"The animal at 0 is the 1st and is a  {animals[0]}")
+print(f"The animal at 2 is the 2nd and is a  {animals[2]}")
+print(f"The animal at 3 is the 3rd and is a  {animals[3]}")
+print(f"The animal at 4 is the 4th and is a  {animals[4]}")
+print(f"The animal at 5 is the 5th and is a  {animals[5]}")
